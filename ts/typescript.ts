2c80443627d09ae5d813790cafe6878767bc1d44
Arnab Das <arnab@coderuse.com>
2016-05-30 13:31:36 +0530

    Objects and interfaces in Typescript

--- ts/typescript.ts
+++ ts/typescript.ts
@@ -1,9 +1,13 @@
+// Interface
+// In interface short form for a function type can be used
 interface IStudent {
   firstName: string;
   lastName: string;
   greet(): void;
 };
 
+// Objects implementing an interface must implement all the properties
+// No extra properties are allowed
 var student: IStudent = {
   firstName: 'John',
   lastName: 'Doe',
@@ -12,15 +16,12 @@ var student: IStudent = {
   }
 };
 
-var graduateStudent = {
-  firstName: "Samara",
-  lastName: "Braithweight"
-}
-
+// It's also possible to declare the type of a parameter as an interface
 function doSomething(obj: IStudent) {
 
 }
 
+// And we have to pass a value with specific properties, described in the interface 
 doSomething({
   firstName: 'Jane',
   lastName: 'Doe',
