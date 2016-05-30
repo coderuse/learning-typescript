b6ec13b1655680e51ff5bba1cafc622b17048513
Arnab Das <arnab@coderuse.com>
2016-05-30 15:26:28 +0530

    Shorthand for adding new properties to classes through constructors

--- ts/typescript.ts
+++ ts/typescript.ts
@@ -1,10 +1,6 @@
 class Student {
-  firstName: string;
-  lastName: string;
   
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+  constructor(public firstName: string, public lastName: string) {
   }
   
   greet() {
